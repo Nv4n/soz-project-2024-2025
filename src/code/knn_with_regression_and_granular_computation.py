f8065bf3ce7af2ea7ff55e798c366b8730ad43e6
Sybatron <vipsister2014@gmail.com>
2025-01-22 16:40:56 +0200

    fix: removed unused lib packages

--- src/code/knn_with_regression_and_granular_computation.py
+++ src/code/knn_with_regression_and_granular_computation.py
@@ -2,7 +2,6 @@
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split
 from scipy.stats import pearsonr
 import warnings
